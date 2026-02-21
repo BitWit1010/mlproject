eac70a16007da0adf9be84500de660384c3c03e8
BitWit1010 <himanshu.singh.jmd@gmail.com>
2026-02-21 18:09:28 +0530

    Building Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -42,8 +42,7 @@ class DataTransformation:
             cat_pipeline=Pipeline(
                 steps=[
                     ('imputer',SimpleImputer(strategy='most_frequent')),
-                    ('one_hot_encoder',OneHotEncoder()),
-                    ('scaler',StandardScaler())
+                    ('one_hot_encoder',OneHotEncoder(handle_unknown='ignore'))
                 ]
             )
 
